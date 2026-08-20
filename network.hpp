70653e0405f351cda5652680ccef602e8a03c4f9
NepoOwen <linus.v.sjo@gmail.com>
2026-08-20 20:04:06 +0200

    Remove comments and clean up network.hpp

--- network.hpp
+++ network.hpp
@@ -31,14 +31,6 @@ SOFTWARE.
 #include "utils/memory_utils.hpp"
 #include <cstring>
 
-// types enum
-enum class NetworkParamType {
-    Int,
-    Int64,
-    String,
-    Byte
-};
-
 namespace structs {
 
     struct BufferStruct {
